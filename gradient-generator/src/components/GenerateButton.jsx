8b819ae3c1d5606bd57d11533b375418903ecdcb
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-09-07 14:29:59 +0200

    fixed css, just state of color label left

--- gradient-generator/src/components/GenerateButton.jsx
+++ gradient-generator/src/components/GenerateButton.jsx
@@ -1,34 +1,28 @@
 import { useState } from "react";
 import ColorButton from "./ColorButton";
 import DirectionButtons from "./DirectionButtons";
+import "./GenerateButton.css";
 
 function GenerateButton() {
   const [gradient, setGradient] = useState("");
   const [color, setColor] = useState("#000000");
   const [color2, setColor2] = useState("#0000000");
   const [gradientAngle, setGradientAngle] = useState(0);
-  // const [selectedDirection, setSelectedDirection] = useState("top");
 
-  // const handleColor1Change = (event) => {
-  //   setColor(event.target.value);
-  // };
-  // const handleColor2Change = (event) => {
-  //   setColor2(event.target.value);
-  // };
   const generateGradient = () => {
     const newGradient = `linear-gradient(${gradientAngle}deg, ${color}, ${color2})`;
     setGradient(newGradient);
   };
   return (
-    <>
+    <div className="gradient-app-block">
       <body style={{ background: gradient }}>
-        <div>
+        <div className="direction-div">
           <DirectionButtons setGradientAngle={setGradientAngle} />
         </div>
-        <div>
+        <div className="color-div">
           <ColorButton setColor={setColor} setColor2={setColor2} />
         </div>
-        <div>
+        <div className="generate-div">
           <button
             className="generate-button"
             type="submit"
@@ -37,16 +31,19 @@ function GenerateButton() {
             Generate
           </button>
         </div>
-        <div>
-          <p>Selected Direction: {gradientAngle}</p>
-          <p>Selected Color 1: {color}</p>
-          <p>Selected Color 2: {color2}</p>
-          <p>
+        <div className="infos-div">
+          <p className="infos-p sd">Selected Direction:</p>
+          <p className="infos-user ga"> {gradientAngle}</p>
+          <p className="infos-p sc">Selected Color 1: </p>
+          <p className="infos-user c1">{color}</p>
+          <p className="infos-p sc2">Selected Color 2: </p>
+          <p className="infos-user c2">{color2}</p>
+          <p className="infos-p r">
             linear-gradient({gradientAngle}deg,{color},{color2})
           </p>
         </div>
       </body>
-    </>
+    </div>
   );
 }
 
