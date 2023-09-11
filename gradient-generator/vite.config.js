d3beb7ee577fd9ad10a9497a6923d0342c08fa2b
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-09-11 17:08:22 +0200

    change viteconfig

--- gradient-generator/vite.config.js
+++ gradient-generator/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/gradient-generator",
+  base: "/gradient_generator",
   plugins: [react()],
 });
