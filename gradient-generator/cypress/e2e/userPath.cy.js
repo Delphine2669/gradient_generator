0f5c3a3300179d5fddbfb3fb7ffe2b18403ab9d6
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-09-11 16:50:56 +0200

    trying to deploy

--- gradient-generator/cypress/e2e/userPath.cy.js
+++ gradient-generator/cypress/e2e/userPath.cy.js
@@ -2,7 +2,8 @@ describe("user path", () => {
   it("it should show the site homepage", () => {
     cy.visit("http://localhost:5174/");
     cy.get(".top-button").should("have.text", "Top").click();
-    cy.get(".color-button1").click().click("center");
+    cy.get(".color-button1").click();
+    cy.get(".color-button1").click("center");
     cy.get(".color-button1-label").should("have.text", "#000000");
     cy.get(".generate-button").should("have.text", "Generate").click();
   });
