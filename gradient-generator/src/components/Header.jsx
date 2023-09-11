bb080ee8def8b9085cf1104e6373ec325be71ff6
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-09-11 18:57:21 +0200

    adding things for SEO

--- gradient-generator/src/components/Header.jsx
+++ gradient-generator/src/components/Header.jsx
@@ -1,6 +1,8 @@
+import logo from "../assets/logo.svg";
 function Header() {
   return (
     <div className="gradient-header">
+      <img src={logo} alt="logo gradient generator" className="logo" />
       <h1>Gradient Generator</h1>
     </div>
   );
